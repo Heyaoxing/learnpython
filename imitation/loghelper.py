d9e909cd09abcbeaeffb1f8cb1514b314d26194a
hyx <730530507@qq.com>
2018-04-23 19:33:47 +0800

    1

--- imitation/loghelper.py
+++ imitation/loghelper.py
@@ -1,15 +1,25 @@
+
 import logging
 import sys
 
 logging.basicConfig(level=logging.DEBUG,
-                    filename='new.log',
+                    filename='complete.log',
                     filemode='a',
                     format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 logger = logging.getLogger('simple_logger')
 logger.setLevel(logging.DEBUG)
- 
-logger.debug('debug message')
-logger.info('info message')
-logger.warn('warn message')
-logger.error('error message')
-logger.critical('critical message')
+
+
+def debug(content):
+    logger.debug(content)
+
+def info(content):
+    logger.info(content)
+
+def warn(content):
+    logger.warn(content)
+
+def error(content):
+    logger.error(content)
+
+    

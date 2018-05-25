5b0501527d163b57ca644540791b051000b3d789
990016 <heyaoxing@fcbox.com>
2018-05-25 15:47:40 +0800

    1

--- imitation/selectten.py
+++ imitation/selectten.py
@@ -50,7 +50,7 @@ def filter(code, hist, temp,star,end):
         if df.iloc[index+1]['volume']>df.iloc[index]['volume']:
             index=index+1
             continue
-        if df.iloc[index]['ma5']<df.iloc[index+1]['ma5'] and df.iloc[index]['ma10']<df.iloc[index+1]['ma10']:    
+        if df.iloc[index]['ma5']>df.iloc[index+1]['ma5'] and df.iloc[index]['ma10']>df.iloc[index+1]['ma10'] and df.iloc[index]['ma20']>df.iloc[index+1]['ma20']:    
             isTrue=True  
         index=index+1
 
